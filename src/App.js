2135e017b21b8429b0c8bc78fd389b552b760a89
Alex Persinger <apfbacc@gmail.com>
2022-02-15 22:00:45 -0500

    Initial Commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import UserInput from "./bungo";
+
+const bungo_link = 'https://www.bungie.net/en/OAuth/Authorize?client_id=39340&response_type=code&state=3g5h2hbvndb08'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput/>
     </div>
   );
 }
---
+++ src/bungo.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import ButtonComponent from "./components/general/Button";
+import { v4 as uuidv4 } from 'uuid';
+
+const bungo_link = 'https://www.bungie.net/en/OAuth/Authorize?client_id=39340&response_type=code&state='
+
+const UserInput = () => {
+    // the key with this type of setup is that we need a line & f(x) for each input
+    const [inVal, setVal] = useState("");
+
+    useEffect(() => {
+        setUrl(window.location.href)
+    },[])
+
+    const setUrl = (value) => {
+        console.log(value);
+        setVal(value);
+    }
+
+    const submitValue = () => {
+        const frmdetails = {
+            'First Name' : inVal
+        }
+        console.log(frmdetails);
+    }
+
+    const testing = () => {
+        let stateVal = uuidv4().replaceAll('-', '')
+        console.log(stateVal)
+    }
+
+    const goToBungo = () => {
+        let stateVal = uuidv4().replaceAll('-', '')
+        window.location.href=bungo_link+stateVal;
+    }
+
+    return (
+        <div>
+            <ButtonComponent onClickFunction={goToBungo} label={"Authorize"}/>
+            <ButtonComponent onClickFunction={testing} label={"Test"}/>
+            <div>
+                {inVal}
+            </div>
+        </div>
+    )
+}
+
+
+
+export default UserInput;
