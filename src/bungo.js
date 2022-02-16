56f2f4202d16ea8e65beb4a18256c1ea8038649f
Alex Persinger <apfbacc@gmail.com>
2022-02-16 15:15:12 -0500

    again

--- src/bungo.js
+++ src/bungo.js
@@ -22,6 +22,9 @@ const UserInput = () => {
     // the key with this type of setup is that we need a line & f(x) for each input
     const [authCode, setAuthCode] = useState("");
     const [uuidState, setUUIDState] = useState("");
+    const [token, setToken] = useState("");
+    const [memershipType, setMemberType] = useState("");
+    const [membershipId, setMemberId] = useState("");
     const bungo_link = 'https://www.bungie.net/en/OAuth/Authorize?client_id=39340&response_type=code&state='
 
     useEffect(() => {
@@ -41,8 +44,7 @@ const UserInput = () => {
     }
 
     const testing = () => {
-        let stateVal = uuidv4().replaceAll('-', '')
-        console.log(stateVal)
+        setToken('CNXyAxKGAgAgHfmll6fhppjEK7J+R4Tg0c+kLB2LvABRzzc0IJ7w/q/gAAAAk658cDZuuPwFZLj+YFZ780tO+NVVnHT8zEaV792IsWdQi5NTL50xcfVHfa5DL6mZUuHiB+Ssw2GKp+a46JQvcxI5umdEqjZvwNBBW3kksegnerdO6AHNHsQkyA8l3w79dVbtRNQz/RX28BW162sxDz/XjFsUSDf4jATE9ITgah6Odr8cSITen92vBIRmoN4H/CEN6qMOfWNXvuQH9NIFeq2N3ds5Cx4CXRyGQKmQ0ZsmTqKE+9r67Pv7eas2vi8baOYxhBUe0ZvQLt3vk496EvCcU9Elpy1s94lDKlinaPQ=')
     }
 
     const goToBungo = () => {
@@ -70,21 +72,59 @@ const UserInput = () => {
             data : data
         }
         const res = await axios(config);
-        console.log(res);
+        console.log(res.data['access_token'])
+        setToken(res.data['access_token'])
+    }
+
+    const getCurrentUserMembership = async () => {
+        var config = {
+            method: 'get',
+            url: 'https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/',
+            headers: {
+                'x-api-key': 'a13c1aa6c60c44129469453f9513a412',
+                'Authorization': 'Bearer ' + token
+            }
+        };
+
+        const res = await axios(config);
+        setMemberType('3');
+        setMemberId('4611686018471219555')
+        console.log(res)
+    }
+
+    const getUserInv = async () => {
+        var config = {
+            method: 'get',
+            url: 'https://www.bungie.net/Platform/Destiny2/' + memershipType + '/Profile/' + membershipId + '/?components=102',
+            headers: {
+                'x-api-key': 'a13c1aa6c60c44129469453f9513a412',
+                'Authorization': 'Bearer ' + token
+            }
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     return (
         <div>
-            <ButtonComponent onClickFunction={goToBungo} label={"Authorize"}/>
-            <ButtonComponent onClickFunction={reauth} label={"Reauthorize"}/>
-            <ButtonComponent onClickFunction={getToken} label={"Get Token"}/>
+            <ButtonComponent onClickFunction={goToBungo} label={"1. Authorize"}/>
+            <ButtonComponent onClickFunction={reauth} label={"1b. Reauthorize"}/>
+            <ButtonComponent onClickFunction={getToken} label={"2. Get Token"}/>
+            <ButtonComponent onClickFunction={getCurrentUserMembership} label={"3. Get User Membership"}/>
+            <ButtonComponent onClickFunction={getUserInv} label={"4. Get User Inventory"}/>
             <ButtonComponent onClickFunction={testing} label={"Test"}/>
             <div>
                 <p>
                     Auth Code: {authCode}
                 </p>
                 <p>
-                    Token: {authCode}
+                    Token: {token}
                 </p>
                 <p>
                     UUID State: {uuidState}
